d4b37a3402af723543a22e15c7ad6cb80d4cd955
jorgeluis098 <jorge.luis.rb.98@gmail.com>
2020-12-07 23:49:54 -0600

    se agrego la la navbar asi como su funcionalidad

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/nav")
+def navegation():
+    return render_template("nav.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
